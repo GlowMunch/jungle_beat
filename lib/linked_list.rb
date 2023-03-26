1724020b137aeba5cc5bb98c2d708ff9c043ebe0
Blaine Glasgow <glasgowb@outlook.com>
2023-03-26 12:19:10 -0700

    Complete the method append

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,30 +1,31 @@
 class LinkedList
-  attr_accessor :head, :append, :count
+  attr_accessor :head, :append#, :count
 
-  def initialize(head = nil)
-    @head = head
+  def initialize 
+    @head = nil
   end
 
   def append(sound)
     if head == nil
-      @head = Node.new(sound)
+      @head = Node.new(sound, nil)
     else
-      while head != nil
-        head = @head.next_node
+      last_node = @head
+      while last_node.next_node != nil
+        last_node = last_node.next_node
       end
-        @head.next_node = Node.new(sound, nil)
+        last_node.next_node = Node.new(sound, nil)
     end
   end
   
-  def count
-    count = 0
-    current_node = @head
-    while current_node.next_node != nil
-      current_node = current_node.next_node
-      count += 1
-    end
-      count += 1
-  end
+  # def count
+  #   count = 0
+  #   current_node = @head
+  #   while current_node.next_node != nil
+  #     current_node = current_node.next_node
+  #     count += 1
+  #   end
+  #     count += 1
+  # end
 end
 
 
