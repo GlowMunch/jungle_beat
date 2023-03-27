25e164cbd88dd14b752dab775d390d8379917d60
Blaine Glasgow <glasgowb@outlook.com>
2023-03-27 14:10:17 -0700

    Add method(s): prepend, count, to_string

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,9 +1,11 @@
 class LinkedList
-  attr_accessor :head, :append#, :count
+  attr_accessor :head, :append, :count, :to_string, :prepend
 
   def initialize 
     @head = nil
   end
+  
+#what is the diff between list.head and @head in pry
 
   def append(sound)
     if head == nil
@@ -17,15 +19,40 @@ class LinkedList
     end
   end
   
-  # def count
-  #   count = 0
-  #   current_node = @head
-  #   while current_node.next_node != nil
-  #     current_node = current_node.next_node
-  #     count += 1
-  #   end
-  #     count += 1
-  # end
+  def count
+    count = 0
+    current_node = @head
+    while current_node.next_node != nil
+      current_node = current_node.next_node
+      count += 1
+    end
+      count += 1
+  end
+
+  def to_string
+    string = ""
+    current_node = @head
+    while current_node != nil
+      string << current_node.data + " "
+      current_node = current_node.next_node
+    end
+      string.chomp(" ")
+  end
+
+  def prepend(data)
+    if head != nil
+      old_head = head
+      @head = Node.new(data, nil)
+      head.next_node = old_head
+    else head == nil
+      @head = Node.new(data, nil)
+    end
+  end
+
+  def insert(data)
+    
+  end
+
 end
 
 
